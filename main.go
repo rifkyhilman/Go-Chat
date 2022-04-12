57e10490b2c9ea5baebe3981ebdb92d527860320
rifkyhilman <rifkyhilman265@gmail.com>
2022-04-12 15:07:01 +0700

    Hello Go Web git add .

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "Welcome to Golang"
+
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello Web !!"
+
+	w.Write([]byte(message))
+}
 
 func main() {
-	var angka int = 10
-	var kalimat string = "April"
-	tahun := 2022
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
 
-	fmt.Println("Go Learn !!")
-	fmt.Println(angka, kalimat, tahun)
+	var address = "localhost:9000"
+	fmt.Printf("server Berjalan di :%s\n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
