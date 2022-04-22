7939ef77a37f05b1572ea9d3b47ce0394e9bba36
rifkyhilman <rifkyhilman265@gmail.com>
2022-04-22 22:59:50 +0700

    edit Frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+    <div ClassName="header">
+        <h2>RealTime Chat App</h2>
+    </div>
+)
+
+export default Header
